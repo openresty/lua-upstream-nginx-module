616469b189b8cdc577cc30db9dbc6bf0d3b49c62
jiahao <wangjiahao@openresty.com>
2024-08-07 12:59:16 +0800

    tests: use ngx_http_redis from openresty/ngx_http_redis instead.

--- util/build.sh
+++ util/build.sh
@@ -9,41 +9,10 @@ version=$1
 home=~
 force=$2
 
-ngx_redis_version=0.3.9
-cd $home/work/nginx/ || exit 1
-ngx_redis_path=$home/work/nginx/ngx_http_redis-$ngx_redis_version
-rm -rf $ngx_redis_path || exit 1
-tar -xzvf ngx_http_redis-$ngx_redis_version.tar.gz || exit 1
-
-cd $ngx_redis_path || exit 1
-
-patch_file=$root/../openresty/patches/ngx_http_redis-$ngx_redis_version-variables_in_redis_pass.patch
-if [ ! -f $patch_file ]; then
-    echo "$patch_file: No such file" > /dev/stderr
-    exit 1
-fi
-# we ignore any errors here since the target directory might have already been patched.
-patch -p1 < $patch_file || exit 1
-
-cd $ngx_redis_path || exit 1
-
-patch_file=$root/../openresty/patches/ngx_http_redis-$ngx_redis_version-default_port_fix.patch
-if [ ! -f $patch_file ]; then
-    echo "$patch_file: No such file" > /dev/stderr
-    exit 1
-fi
-# we ignore any errors here since the target directory might have already been patched.
-patch -p1 < $patch_file || exit 1
-
 disable_pcre2="";
-patch_file=$root/../openresty/patches/ngx_http_redis-$ngx_redis_version-remove_content_encoding.patch
 answer=`$root/../openresty/util/ver-ge "$version" 1.23.0`
 if [ "$answer" = "Y" ]; then
     disable_pcre2=--without-pcre2;
-    echo
-    echo "applying ngx_http_redis-$ver-remove_content_encoding.patch"
-    patch -p1 < $patch_file || exit 1
-    echo
 fi
 
 cd $root || exit 1
@@ -64,7 +33,7 @@ ngx-build $force $version \
             --without-http_userid_module \
             --add-module=$root/../ndk-nginx-module \
             --add-module=$root/../set-misc-nginx-module \
-          --add-module=$ngx_redis_path \
+          --add-module=$root/../ngx_http_redis \
           --add-module=$root/../echo-nginx-module \
           --add-module=$root $opts \
           --add-module=$root/../lua-nginx-module \
